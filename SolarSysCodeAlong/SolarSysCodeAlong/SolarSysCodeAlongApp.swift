25d59bffc75cb591b2a19ad16354840c9c4f1f54
Ace <ace.authors@rmit.edu.au>
2025-09-16 06:21:45 +1000

    Init

---
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSysCodeAlongApp.swift
+//  SolarSysCodeAlong
+//
+//  Created by Ace on 15/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct SolarSysCodeAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
