4f4b03a8129ee6f3bb0d4c9cbf9f86e52a0a77a7
RMIT-Ace <ace.authors@rmit.edu.au>
2025-09-16 11:48:24 +1000

    WIP 01-BasicEntity

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -7,11 +7,51 @@
 
 import SwiftUI
 import RealityKit
+import SolarSysRealityKit
 
 struct SolarSysCodeAlongView: View {
+    let depth: Float = -0.5
+    
     var body: some View {
         RealityView { content in
             content.camera = .spatialTracking
+            
+            let boxEntity = ModelEntity(
+                mesh: .generateBox( size: 0.25 ),
+                materials: [SimpleMaterial( color: .blue, isMetallic: true )]
+            )
+            boxEntity.transform = Transform(translation: SIMD3(0, 0, depth))
+            content.add(boxEntity)
+            
+            let sphereEntity = ModelEntity(
+                mesh: .generateSphere(radius: 0.25),
+                materials: [SimpleMaterial(color: .red, isMetallic: true)]
+            )
+            sphereEntity.transform = Transform(translation: SIMD3(1, 0, depth))
+            content.add(sphereEntity)
+            
+            let startfieldTexture = try? await TextureResource(
+                named: "starfield",
+                in: SolarSysRealityKitResources.bundle,
+            )
+            if startfieldTexture != nil {
+                let mesh = MeshResource.generateBox(size: 0.5)
+                let material = UnlitMaterial(texture: startfieldTexture!)
+                let boxWithStars = ModelEntity(mesh: mesh, materials: [material])
+                boxWithStars.transform = Transform(translation: SIMD3(-1, 0, depth))
+                content.add(boxWithStars)
+            } else {
+                print("DEBUG: Can't load starfield.")
+            }
+            
+            if let hapiLabTexture = try? await TextureResource(named: "HAPI-lab", in: SolarSysRealityKitResources.bundle) {
+                let mesh = MeshResource.generateSphere(radius: 0.25)
+                let material = UnlitMaterial(texture: hapiLabTexture)
+                let hapiSphere = ModelEntity(mesh: mesh, materials: [material])
+                hapiSphere.transform = Transform(translation: SIMD3(0, 0.5, depth))
+                content.add(hapiSphere)
+
+            }
         }
         .ignoresSafeArea()
     }
