7e41d91b64fec6cc802bf27c39d2c2ee23fa3851
Ace <ace.authors@rmit.edu.au>
2025-09-21 07:14:30 +1000

    Started mvvm-1

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -34,36 +34,11 @@ struct SolarSysCodeAlongView: View {
             content.add(root)
             root.position.z = -1.0
             
-            // 3D Model - Sun
-            sun = await CelestialEntity(
-                bundle: SolarSysRealityKitResources.bundle,
-                name: "Sun",
-                scale: 3.0,
-                distanceFromCenter: 0.0)
-            if let sun = sun {
-                root.addChild(sun)
-            }
-
-            // 3D Model - Earth
-            earth = await CelestialEntity(
-                bundle: SolarSysRealityKitResources.bundle,
-                name: "Earth",
-                scale: 1.0,
-                distanceFromCenter: 1.0)
-            if let earth = earth, let sun = sun {
-                sun.addChild(earth)
-            }
+            await addCelestialEntity(
+                to: root,
+                celestialObj: vm.celestialObjects.first
+            )
             
-            // 3D Model - Moon
-            moon = await CelestialEntity(
-                bundle: SolarSysRealityKitResources.bundle,
-                name: "Moon",
-                scale: 1.0 / 2.0,
-                distanceFromCenter: 0.2)
-            if let earth = earth, let moon = moon {
-                earth.addChild(moon)
-            }
-
         } update: { content in
             Task {
                 await sun?.updateRotation(speed: standardSpeed / 27.0)  // 27 Earth-day
@@ -79,6 +54,26 @@ struct SolarSysCodeAlongView: View {
         .ignoresSafeArea()
     }
     
+    private func addCelestialEntity(
+        to parent: CelestialEntity,
+        celestialObj: CelestialObject?
+    ) async {
+        guard let celestialObj = celestialObj else { return }
+        guard let newCelestialEntity = await CelestialEntity(
+            bundle: SolarSysRealityKitResources.bundle,
+            name: celestialObj.name,
+            scale: celestialObj.scale,
+            distanceFromCenter: celestialObj.distanceCenter
+        )  else {
+            print(">> WARN: Could not create model for \(celestialObj.name)")
+            return
+        }
+        parent.addChild(newCelestialEntity)
+        for child in celestialObj.satellites {
+            await addCelestialEntity( to: newCelestialEntity, celestialObj: child )
+        }
+    }
+        
     private func makeSkybox(_ content: RealityViewCameraContent) async {
         // Skybox
         if let hapiLabTexture = try? await TextureResource(named: "starfield", in: SolarSysRealityKitResources.bundle) {
@@ -95,3 +90,4 @@ struct SolarSysCodeAlongView: View {
     SolarSysCodeAlongView()
         .environment(SolarSysViewModel())
 }
+
