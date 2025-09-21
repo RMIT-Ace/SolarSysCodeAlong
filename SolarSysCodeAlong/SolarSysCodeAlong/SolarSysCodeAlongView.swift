3091b709d9faccfae1a910bd32b9e94a33b28737
Ace <ace.authors@rmit.edu.au>
2025-09-21 15:51:36 +1000

    Completed exercises

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -4,18 +4,31 @@
 //
 //  Created by Ace on 15/9/2025.
 //
+//  Notes:
+//  (1) Simple Box shape with ModelEntity() - mesh and materials
+//  (1a) Depth (meter) - distance away from camera for easy viewing
+//  (2) Simple Sphere shape. Note on translation to move object from overlapping each others.
+//  (3) Simple box entity with custom texture.
+//  (3a) Import SolarSysRealityKet provides extra pre-made 3d assets and resources.
+//  (4) Create 3D entity as Skybox! Note on a) Radius, b) Negative scale
+//  (5) Load pre-made 3D model, i.e. Earth. Note on getting resource from SolarSysRealityKit package
+//
+//  Exercises:
+//  (E1) Playaround with different textures
+//  (E2) Experiment with different negative scale for skybox.
 
 import SwiftUI
 import RealityKit
-import SolarSysRealityKit
+import SolarSysRealityKit       // (3a)
 
 struct SolarSysCodeAlongView: View {
-    let depth: Float = -0.5
+    let depth: Float = -0.5     // (1a)
     
     var body: some View {
         RealityView { content in
             content.camera = .spatialTracking
             
+            // (1) _solarsimplebox
             let boxEntity = ModelEntity(
                 mesh: .generateBox( size: 0.25 ),
                 materials: [SimpleMaterial( color: .blue, isMetallic: true )]
@@ -23,6 +36,7 @@ struct SolarSysCodeAlongView: View {
             boxEntity.transform = Transform(translation: SIMD3(0, 0, depth))
             content.add(boxEntity)
             
+            // (2) _solarsimplesphere
             let sphereEntity = ModelEntity(
                 mesh: .generateSphere(radius: 0.25),
                 materials: [SimpleMaterial(color: .red, isMetallic: true)]
@@ -30,6 +44,7 @@ struct SolarSysCodeAlongView: View {
             sphereEntity.transform = Transform(translation: SIMD3(1, 0, depth))
             content.add(sphereEntity)
             
+            // (3) _solarboxstar
             let startfieldTexture = try? await TextureResource(
                 named: "starfield",
                 in: SolarSysRealityKitResources.bundle,
@@ -44,8 +59,11 @@ struct SolarSysCodeAlongView: View {
                 print("DEBUG: Can't load starfield.")
             }
             
-            // Skybox
-            if let hapiLabTexture = try? await TextureResource(named: "HAPI-lab", in: SolarSysRealityKitResources.bundle) {
+            // (4) _solarsimpleskybox
+            if let hapiLabTexture = try? await TextureResource(
+                named: "puresky",
+                in: SolarSysRealityKitResources.bundle
+            ) {
                 let mesh = MeshResource.generateSphere(radius: 10)
                 let material = UnlitMaterial(texture: hapiLabTexture)
                 let hapiSphere = ModelEntity(mesh: mesh, materials: [material])
@@ -54,8 +72,11 @@ struct SolarSysCodeAlongView: View {
                 hapiSphere.transform.scale = SIMD3(-1, 1, 1)
             }
             
-            // 3D Model
-            if let url = SolarSysRealityKitResources.bundle.url(forResource: "Earth", withExtension: "usdz"),
+            // (5) _solar3dmodel
+            if let url = SolarSysRealityKitResources.bundle.url(
+                forResource: "Earth",
+                withExtension: "usdz"
+            ),
                let earth = try? await ModelEntity(contentsOf: url) {
                 earth.transform = Transform(translation: SIMD3(0, -0.5, depth))
                 content.add(earth)
