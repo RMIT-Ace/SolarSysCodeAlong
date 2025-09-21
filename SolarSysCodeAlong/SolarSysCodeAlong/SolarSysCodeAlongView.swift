0883441d9f1533a901d1b97b2dc9114839f050b4
Ace <ace.authors@rmit.edu.au>
2025-09-21 14:26:14 +1000

    Commenting for presentation

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -4,16 +4,21 @@
 //
 //  Created by Ace on 15/9/2025.
 //
+//  Notes:
+//  (1) Minimum project structure for RealityKit app.
+//  (2) SwiftUI component - RealityView
+//  (3) Camera tracking - mapping virtual entities into realworld.
+//  (4) Expand our view on entire display areas.
 
 import SwiftUI
 import RealityKit
 
-struct SolarSysCodeAlongView: View {
+struct SolarSysCodeAlongView: View {                // (1)
     var body: some View {
-        RealityView { content in
-            content.camera = .spatialTracking
+        RealityView { content in                    // (2)
+            content.camera = .spatialTracking       // (3)
         }
-        .ignoresSafeArea()
+        .ignoresSafeArea()                          // (4)
     }
 }
 
