7a88bb8f63efee013a966acd46f52dea63e3e8c0
RMIT-Ace <ace.authors@rmit.edu.au>
2026-01-06 13:08:25 +1100

    Added

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -2,28 +2,11 @@
 //  ContentView.swift
 //  SolarSysCodeAlong
 //
-//  Created by Ace on 15/9/2025.
+//  Created by Ace on 06/01/2026
 //
 //  Notes:
-//  (1) Remove all codes from previous exercise, except skybox code.
-//  (2) Refactor code that creates skybox into a resuable function.
-//  (2a) Call skybox function to create skybox for our code.
-//  (3) Create entities
-//  (3a) Big blue box entity
-//  (3b) Earth entity
-//  (3c) Moon entity
-//  (3d) Sun entity
-//  (4) Adding Component and System - see:
-//  (4a) RotationComponent and
-//  (4b) RotationSystem
-//  (4c) Register Rotation System
-//  (5) Make entity rotates
-//  (5a) Make blue box rotates
-//  (5b) Make Earth rotates
-//  (5c) Make Sun rotates
+//  Implementing basic animation without using ECS.
 //
-//  Exercises:
-//  (E1) Make the blue box invisible!
 
 import SwiftUI
 import RealityKit
@@ -34,28 +17,43 @@ struct SolarSysCodeAlongView: View {
     let depth: Float = -3.0
     let boxSize: Float = 2.0
     
+    @State private var redBoxRotation: simd_quatf = .init()
+    
     var body: some View {
         RealityView { content in
             content.camera = .spatialTracking
             
-            // (1) Exercise setup
-            
-            // (2a) _solarcallskyboxfunc
-            
-            // (3a) _solarbigbluebox
-            
-            // (5a) _solarblueboxrotate
-            
-            // (3b) _solarblueboxearth
-            
-            // (3d) _solarblueboxsun
+            let redBox = ModelEntity(
+                mesh: .generateBox(size: boxSize),
+                materials: [
+                    SimpleMaterial(
+                        color: .red.withAlphaComponent(0.2), isMetallic: false
+                    )
+                ]
+            )
+            redBox.name = "RedBox"
+            redBox.transform = Transform(translation: SIMD3(0, 0, depth))
+            content.add(redBox)
+            self.redBoxRotation = redBox.transform.rotation
+        } update: { content in
+            if let redBox = content.entities.first(where: {$0.name == "RedBox"}) {
+                redBox.transform.rotation = redBoxRotation
+            }
         }
-        // (4c) _solarregisterrotation
-        
+        .task {
+            await foreverRunloop()
+        }
         .ignoresSafeArea()
     }
     
-    // (2) _solarskyboxfunc
+    // Animation-loop without using ECS
+    private func foreverRunloop() async {
+        let angle: Float = 5 * .pi / 180
+        while true {
+            try? await Task.sleep(for: .milliseconds(100))
+            redBoxRotation *= simd_quatf(angle: angle, axis: [0, 1, 0])
+        }
+    }
 }
 
 #Preview {
