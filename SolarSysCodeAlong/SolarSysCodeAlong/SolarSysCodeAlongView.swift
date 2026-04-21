f8502c4b5055d1cf0464eee99475872efb5c5f40
Ace (P38) <AceP38@afp.rmit.edu.au>
2026-04-21 15:38:36 +1000

    Test commit and push from P38

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -29,6 +29,7 @@ struct SolarSysCodeAlongView: View {
         RealityView { content in
             content.camera = .spatialTracking
 
+            // Test from P38
             // Configure spatial tracking without occlusion so real-world objects don't interfere with skybox
             let config = SpatialTrackingSession.Configuration(
                 tracking: [.camera, .world, .plane, .object, .image],
