701084440600d60717909deae750b932de921b19
RMIT-Ace <ace.authors@rmit.edu.au>
2026-05-01 15:56:00 +1000

    Beginner-00 basic empty

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -5,12 +5,8 @@
 //  Created by Ace on 15/9/2025.
 //
 
-// Beginner-01
-// 1) Put basic entity (sphere) in your real world.
-// 2) Create more spheres of various:
-//      - sizes
-//      - colors
-//      - position (x, y, z)
+// Beginner-00
+// 1) Basic minimum RealityKit app
 
 import SwiftUI
 import RealityKit
@@ -23,20 +19,8 @@ struct SolarSysCodeAlongView: View {
             content.camera = .spatialTracking
             await setCameraTracking()
             
-            // Red sphere at center
-            let redSphereEntity = ModelEntity(
-                mesh: .generateSphere(radius: 0.25),
-                materials: [SimpleMaterial(color: .red, isMetallic: true)]
-            )
-            content.add(redSphereEntity)
             
-            // Blue sphere at center
-            let blueSphereEntity = ModelEntity(
-                mesh: .generateSphere(radius: 0.1),
-                materials: [SimpleMaterial(color: .blue, isMetallic: true)]
-            )
-            blueSphereEntity.position = SIMD3(0, 0, -1)
-            content.add(blueSphereEntity)
+            
         }
         .ignoresSafeArea()
     }
