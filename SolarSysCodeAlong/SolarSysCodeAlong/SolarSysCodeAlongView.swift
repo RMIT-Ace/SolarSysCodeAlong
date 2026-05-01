6f02ace2001d48df27f4acf57faa88d7e8f8495e
RMIT-Ace <ace.authors@rmit.edu.au>
2026-05-01 15:46:30 +1000

    Added fix for camera tracking

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -12,9 +12,13 @@ import SwiftUI
 import RealityKit
 
 struct SolarSysCodeAlongView: View {
+    let trackingSession = SpatialTrackingSession()
+    
     var body: some View {
         RealityView { content in
             content.camera = .spatialTracking
+            await setCameraTracking()
+            
             for _ in 0..<10 {
                 addSphere(
                     to: content,
@@ -25,6 +29,15 @@ struct SolarSysCodeAlongView: View {
             }
         }
         .ignoresSafeArea()
+    }
+    
+    func setCameraTracking() async {
+        let config = SpatialTrackingSession.Configuration(
+            tracking: [.camera, .world, .plane, .object, .image],
+            sceneUnderstanding: [.shadow, .collision, .physics],
+            camera: .back
+        )
+        await trackingSession.run(config)
     }
     
     func addSphere(
