ec9a0e37c755e9eba2f72d9bdd1022a3eed1a200
RMIT-Ace <ace.authors@rmit.edu.au>
2026-05-03 11:09:29 +1000

    Adjusted rotation speed

--- SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
+++ SolarSysCodeAlong/SolarSysCodeAlong/SolarSysCodeAlongView.swift
@@ -76,7 +76,7 @@ struct SolarSysCodeAlongView: View {
     }
     
     func getRandomRotationSpeed() -> Float {
-        Float.random(in: 1.0...3.0)
+        Float.random(in: 0.5...1.0)
     }
 }
 
